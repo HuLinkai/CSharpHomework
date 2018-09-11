b363aa2bf55d360904187c7e807bae8fe5303713
HuLinkai <798395174@qq.com>
2018-09-12 00:20:37 +0800

    CSharpHomework_1

---
+++ Homework1/Program1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "";
+            double a, b = 0;
+            Console.Write("Please input num(a):");
+            s = Console.ReadLine();
+            a = double.Parse(s);
+            Console.Write("Please input num(b):");
+            s = Console.ReadLine();
+            b = double.Parse(s);
+            double c = a * b;
+            Console.WriteLine("a * b = {0}", c);
+
+
+        }
+    }
+}
